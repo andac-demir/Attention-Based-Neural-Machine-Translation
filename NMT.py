5e313102007ecf3ebb2ebd242f44573a4eb86eba
andac-demir <andacdemir@hotmail.com>
2018-11-26 23:43:09 -0500

    attention based decoder

--- NMT.py
+++ NMT.py
@@ -9,33 +9,47 @@ of the input sequence.
 '''
 import numpy as np
 import fire
-from __future__ import unicode_literals, print_function, division
-from io import open
-import unicodedata
-import string
-import re
 import random
 import torch
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
+from preprocess import readLangs, filterPairs, MAX_LENGTH, SOS_token, EOS_token
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+'''
+    Dataset is prepared as sentences in French to English.
+    To translate from English to French, set reverse True
+'''
+def prepareData(lang1, lang2, reverse=True):
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    pairs = filterPairs(pairs)
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
 
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
-
         self.embedding = nn.Embedding(input_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
 
     def forward(self, input, hidden):
         embedded = self.embedding(input).view(1, 1, -1)
-        output = embedded
-        output, hidden = self.gru(output, hidden)
+        output, hidden = self.gru(embedded, hidden)
         return output, hidden
 
+    # hidden state and the cell state reset to zero for every epoch regardless
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
@@ -44,7 +58,6 @@ class DecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size):
         super(DecoderRNN, self).__init__()
         self.hidden_size = hidden_size
-
         self.embedding = nn.Embedding(output_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
@@ -62,7 +75,8 @@ class DecoderRNN(nn.Module):
 
 
 class AttnDecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, 
+                 max_length=MAX_LENGTH):
         super(AttnDecoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
@@ -79,32 +93,106 @@ class AttnDecoderRNN(nn.Module):
     def forward(self, input, hidden, encoder_outputs):
         embedded = self.embedding(input).view(1, 1, -1)
         embedded = self.dropout(embedded)
-
         attn_weights = F.softmax(
             self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
         attn_applied = torch.bmm(attn_weights.unsqueeze(0),
                                  encoder_outputs.unsqueeze(0))
-
         output = torch.cat((embedded[0], attn_applied[0]), 1)
         output = self.attn_combine(output).unsqueeze(0)
-
         output = F.relu(output)
         output, hidden = self.gru(output, hidden)
-
         output = F.log_softmax(self.out(output[0]), dim=1)
         return output, hidden, attn_weights
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
-def train():
-    pass
 
-def test():
-    pass
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
 
-def translate():
-    pass
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+def tensorsFromPair(pair):
+    input_tensor = tensorFromSentence(input_lang, pair[0])
+    target_tensor = tensorFromSentence(output_lang, pair[1])
+    return (input_tensor, target_tensor)
+
+teacher_forcing_ratio = 0.5
+
+
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, 
+          decoder_optimizer, criterion, max_length=MAX_LENGTH):
+    encoder_hidden = encoder.initHidden()
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+    loss = 0
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+    decoder_hidden = encoder_hidden
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  # Teacher forcing
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()  # detach from history as input
+
+            loss += criterion(decoder_output, target_tensor[di])
+            if decoder_input.item() == EOS_token:
+                break
+    loss.backward()
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+    return loss.item() / target_length
+
+
+def trainIters(encoder, decoder, n_iters, print_every=1000, 
+               plot_every=100, learning_rate=0.01):
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_pairs = [tensorsFromPair(random.choice(pairs))
+                      for i in range(n_iters)]
+    criterion = nn.NLLLoss()
+    for iter in range(1, n_iters + 1):
+        training_pair = training_pairs[iter - 1]
+        input_tensor = training_pair[0]
+        target_tensor = training_pair[1]
+        loss = train(input_tensor, target_tensor, encoder,
+                     decoder, encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
+                                         iter, iter / n_iters * 100, print_loss_avg))
+        if iter % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+
+
 
 if __name__ == "__main__":
     fire.Fire()
--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,13 @@
+'''
+    The full process for preparing the data is:
+        Read text file and split into lines, split lines into pairs
+        Normalize text, filter by length and content
+        Make word lists from sentences in pairs
+'''
 import re
 import unicodedata
+from io import open
+import string
 
 SOS_token = 0
 EOS_token = 1
@@ -13,6 +21,14 @@ eng_prefixes = (
     "they are", "they re "
 )
 
+'''
+    We need a unique index per word to use as the inputs and targets of the 
+    networks later. 
+    Helper class Lang has word → index (word2index) 
+    and index → word (index2word) dictionaries, 
+    as well as a count of each word word2count to use to later replace 
+    rare words.
+'''
 class Lang:
     def __init__(self, name):
         self.name = name
@@ -43,24 +59,26 @@ def unicodeToAscii(s):
         if unicodedata.category(c) != 'Mn'
     )
 
-# Lowercase, trim, and remove non-letter characters
-
+# Lowercase, trim most punctuation, and remove non-letter characters
 def normalizeString(s):
     s = unicodeToAscii(s.lower().strip())
     s = re.sub(r"([.!?])", r" \1", s)
     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
     return s
 
+'''
+    To read the data file we will split the file into lines, 
+    and then split lines into pairs. 
+    The files are all English → Other Language, 
+    so if we want to translate from Other Language → English 
+    use the reverse flag to reverse the pairs.
+'''
 def readLangs(lang1, lang2, reverse=False):
-    print("Reading lines...")
-
     # Read the file and split into lines
-    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+    lines = open('%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
         read().strip().split('\n')
-
     # Split every line into pairs and normalize
     pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
-
     # Reverse pairs, make Lang instances
     if reverse:
         pairs = [list(reversed(p)) for p in pairs]
@@ -69,9 +87,13 @@ def readLangs(lang1, lang2, reverse=False):
     else:
         input_lang = Lang(lang1)
         output_lang = Lang(lang2)
-
     return input_lang, output_lang, pairs
 
+'''
+    Since there are a lot of example sentences and we want to train 
+    something quickly, we trim the data set to only relatively short
+    and simple sentences. Here the maximum length is 10 words.
+'''
 def filterPair(p):
     return len(p[0].split(' ')) < MAX_LENGTH and \
         len(p[1].split(' ')) < MAX_LENGTH and \
